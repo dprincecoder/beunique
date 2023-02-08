88243c4979527dde200ba7d0d6f021294b11d534
Onimisea <onimisea@gmail.com>
2023-02-08 02:57:39 +0100

    Fixed more deployment errors

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>BeUnique | Unique & Affordable</title>
-        <meta name="description" content="The most unique and affordable ecommerce store for females" />
+        <meta
+          name="description"
+          content="The most unique and affordable ecommerce store for females"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="grid place-items-center py-32">
-        <h1 className='text-4xl font-bold font-anybody'>BeUnique</h1>
+        <h1 className="text-4xl font-bold font-anybody">BeUnique</h1>
       </main>
     </>
-  )
+  );
 }
