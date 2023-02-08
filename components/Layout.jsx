b656abc5a3f116f08444e8896a6d9edc52789939
Onimisea <onimisea@gmail.com>
2023-02-08 03:06:16 +0100

    Increased the max-width to 1440px

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <section className="bg-white dark:bg-white text-black dark:text-black w-[100%] min-w-[320px] max-w-[1200px] overflow-hidden">
+    <section className="bg-white dark:bg-white text-black dark:text-black w-[100%] min-w-[320px] overflow-hidden mx-auto">
       <Header />
       <main className="">{children}</main>
       <Footer />
