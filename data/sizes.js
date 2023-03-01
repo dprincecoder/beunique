4824d09cd0c40eef6a030000a85cec5476d36257
GREENFONTS <godwillonyewuchii@gmail.com>
2023-03-01 21:49:32 +0100

    chore: major refactoring

---
+++ data/sizes.js
@@ -0,0 +1,8 @@
+export const sizes = [
+    { id: 1, size: "xs", selected: false },
+    { id: 2, size: "s", selected: false },
+    { id: 3, size: "m", selected: false },
+    { id: 4, size: "lg", selected: false },
+    { id: 5, size: "xl", selected: false },
+    { id: 6, size: "xxl", selected: false },
+  ];
---
+++ data/sortOptions.js
@@ -0,0 +1,30 @@
+export const sortOptions = [
+  {
+    id: 1,
+    option: "Latest",
+    value: "latest",
+    unavailable: true,
+    selected: false,
+  },
+  {
+    id: 2,
+    option: "Oldest",
+    value: "oldest",
+    unavailable: true,
+    selected: false,
+  },
+  {
+    id: 3,
+    option: "Price: High - Low",
+    value: "high_to_low",
+    unavailable: true,
+    selected: false,
+  },
+  {
+    id: 4,
+    option: "Price: Low - High",
+    value: "low_to_high",
+    unavailable: true,
+    selected: false,
+  },
+];
