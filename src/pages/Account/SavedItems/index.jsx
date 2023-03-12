a5f53fc456a61923c137e871bcf10b6fa9701373
dprincecoder <dprincecoder@gmail.com>
2023-03-12 21:44:22 +0100

    fixes

--- src/pages/Account/SavedItems/index.jsx
+++ src/pages/Account/SavedItems/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import "./savedItem.css";
 
-const Saveditems = () => {
-  return <div>Saveditems</div>;
+const SavedItems = () => {
+  const { cart } = useSelector((state) => state.cartItems);
+  return (
+    <div className="account-savedItems-page">
+      <h1 className="p-title">Saved Items</h1>
+      {cart.map((order) => (
+        <div className="account-savedItems" key={order.id}>
+          <div className="account-savedItems-items">
+            <div className="account-savedItems-item-img">
+              <img src={order.img} alt="account-savedItems-item" />
+            </div>
+            <div className="account-savedItems-item-info">
+              <h3 className="account-savedItems-item-name">{order.name}</h3>
+            </div>
+          </div>
+          <div className="account-savedItems-link">
+            <a href="">See details</a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Saveditems;
+export default SavedItems;
--- src/pages/Account/ViewedItems/index.jsx
+++ src/pages/Account/ViewedItems/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import "./viewedItems.css";
 
 const ViewedItems = () => {
-  return <div>ViewedItems</div>;
+  const { cart } = useSelector((state) => state.cartItems);
+  return (
+    <div className="account-viewedItems-page">
+      <h1 className="p-title">Viewed Items</h1>
+      {cart.map((order) => (
+        <div className="account-viewedItems" key={order.id}>
+          <div className="account-viewedItems-items">
+            <div className="account-viewedItems-item-img">
+              <img src={order.img} alt="account-viewedItems-item" />
+            </div>
+            <div className="account-viewedItems-item-info">
+              <h3 className="account-viewedItems-item-name">{order.name}</h3>
+            </div>
+          </div>
+          <div className="account-viewedItems-link">
+            <a href="">See details</a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ViewedItems;
