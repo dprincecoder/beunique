269bd40991d9fc8bfd7a03a6345738020da8bd1d
dprincecoder <dprincecoder@gmail.com>
2023-03-15 01:53:28 +0100

    commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,31 @@
 import { Bag2, Heart } from "iconsax-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { MdClose } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { addToBag } from "../redux/features/bag/bagSlice";
 import priceFormatter from "../utils/priceFormatter";
+import Loader from "./Loader";
 
 const ProductCard = ({ item }) => {
   const dispatch = useDispatch();
+  const [items, setItems] = useState();
 
-  // console.log(item);
+  console.log(item);
+
+  useEffect(() => {
+    setItems(item);
+  }, [item]);
+
+  console.log(items);
 
   const [selectSizeOpen, setSelectSizeOpen] = useState({
     productId: null,
     isSelectSizeOpen: false,
   });
 
-  const [sizes, setSizes] = useState(item.sizes);
+  const sizes = items?.sizes;
   const [size, setSize] = useState(null);
 
   const handleSize = (sizeId) => {
@@ -41,16 +49,16 @@ const ProductCard = ({ item }) => {
     setSelectSizeOpen({ productId: productIdx, isSelectSizeOpen: true });
   };
 
-  const handleAddToBag = (item) => {
+  const handleAddToBag = (itm) => {
     if (size) {
       const bagItem = {
-        id: item.id,
-        name: item.product_name,
-        price: item.price,
+        id: itm?.id,
+        name: itm?.product_name,
+        price: itm?.price,
         size: size,
-        image: item.product_url[0],
-        category_slug: item.category_slug,
-        item_slug: item.slug_name,
+        image: itm?.product_url[0],
+        category_slug: itm?.category_slug,
+        item_slug: itm?.slug_name,
       };
 
       dispatch(addToBag(bagItem));
@@ -68,128 +76,140 @@ const ProductCard = ({ item }) => {
   };
 
   return (
-    <section className="w-full relative inline-block group duration-300 mt-4">
-      {item.new_stock && (
-        <span className="font-inter text-[#fcfcfd] bg-black rounded-full px-3 py-1 text-[12px] md:text-[14px] absolute top-[8px] left-[8px] z-20">
-          New!
-        </span>
-      )}
+    <>
+      {items?.id ? (
+        <section className="w-full relative inline-block group duration-300 mt-4">
+          {item?.new_stock && (
+            <span className="font-inter text-[#fcfcfd] bg-black rounded-full px-3 py-1 text-[12px] md:text-[14px] absolute top-[8px] left-[8px] z-20">
+              New!
+            </span>
+          )}
 
-      <section className="relative">
-        <img
-          src={item.product_url[0]}
-          alt={item.product_name}
-          width={500}
-          height={500}
-          layout="fill"
-          quality={80}
-          loading="eager"
-          className="w-full h-[240px] sm:h-[300px] sm3:h-[375px] md:h-[300px] md2:h-[270px] lg:h-[340px] lg3:h-[375px] z-10"
-        />
-        <button
-          className="bg-black rounded-lg font-inter text-[14px] text-white px-5 py-2 w-[70%] hidden md:group-hover:block mx-auto cursor-pointer absolute bottom-[25px] left-[50%] -translate-x-[50%] duration-300 "
-          onClick={() => handleSelectSizeOpen(item.id)}
-        >
-          Quick Add
-        </button>
-
-        <section
-          className={`bg-white rounded-full font-inter text-[14px] text-black w-[40px] h-[40px] mx-auto grid cursor-pointer absolute bottom-[8px] right-[8px] duration-300 place-items-center md:hidden ${
-            selectSizeOpen.productId &&
-            selectSizeOpen.isSelectSizeOpen &&
-            "hidden"
-          }`}
-          onClick={() => handleSelectSizeOpen(item.id)}
-        >
-          <Bag2 variant="Linear" size={28} />
-        </section>
-
-        <section
-          className={`absolute bottom-[15px] left-[50%] md:bottom-[25px] md:left-[50%] -translate-x-[50%] duration-300 bg-white w-[90%] md:w-[75%] rounded-lg font-inter p-4 text-black ${
-            selectSizeOpen.productId && selectSizeOpen.isSelectSizeOpen
-              ? "block"
-              : "hidden"
-          } `}
-        >
-          <section className="flex items-center justify-between">
-            <h4 className="text-[14px] text-[#101828]">Select size</h4>
-            <MdClose
-              size={20}
-              className="text-[#1d2939]"
-              onClick={() =>
-                setSelectSizeOpen({ productId: null, isSelectSizeOpen: false })
-              }
+          <section className="relative">
+            <img
+              src={items?.product_url[0]}
+              alt={items?.product_name}
+              width={500}
+              height={500}
+              layout="fill"
+              quality={80}
+              loading="eager"
+              className="w-full h-[240px] sm:h-[300px] sm3:h-[375px] md:h-[300px] md2:h-[270px] lg:h-[340px] lg3:h-[375px] z-10"
             />
-          </section>
+            <button
+              className="bg-black rounded-lg font-inter text-[14px] text-white px-5 py-2 w-[70%] hidden md:group-hover:block mx-auto cursor-pointer absolute bottom-[25px] left-[50%] -translate-x-[50%] duration-300 "
+              onClick={() => handleSelectSizeOpen(items?.id)}
+            >
+              Quick Add
+            </button>
 
-          <section className="border-none w-fit sm2:w-full grid grid-cols-3 gap-5 sm2:gap-3 my-4">
-            {sizes
-              ? sizes.map((size) => {
-                  if (size.selected) {
-                    return (
-                      <section
-                        key={size.id}
-                        className="grid place-items-center  cursor-pointer text-[16px] font-medium w-[40px] h-[40px] bg-[#101828] text-[#fcfcfd] rounded-lg"
-                        onClick={() => handleSize(size.id)}
-                      >
-                        {size.size}
-                      </section>
-                    );
-                  } else if (size.disabled) {
-                    return (
-                      <section
-                        key={size.id}
-                        className={
-                          "grid place-items-center border-[1px] border-[#d0d5dd] cursor-not-allowed text-[16px] font-medium w-[43px] h-[40px] bg-[#eaecf0] text-[#667085] rounded-lg"
-                        }
-                      >
-                        {size.size}
-                      </section>
-                    );
-                  } else {
-                    return (
-                      <section
-                        key={size.id}
-                        className={
-                          "grid place-items-center border-[1px] border-[#d0d5dd] cursor-pointer text-[16px] font-medium w-[43px] h-[40px] bg-white rounded-lg"
-                        }
-                        onClick={() => handleSize(size.id, item.id)}
-                      >
-                        {size.size}
-                      </section>
-                    );
+            <section
+              className={`bg-white rounded-full font-inter text-[14px] text-black w-[40px] h-[40px] mx-auto grid cursor-pointer absolute bottom-[8px] right-[8px] duration-300 place-items-center md:hidden ${
+                selectSizeOpen.productId &&
+                selectSizeOpen.isSelectSizeOpen &&
+                "hidden"
+              }`}
+              onClick={() => handleSelectSizeOpen(items?.id)}
+            >
+              <Bag2 variant="Linear" size={28} />
+            </section>
+
+            <section
+              className={`absolute bottom-[15px] left-[50%] md:bottom-[25px] md:left-[50%] -translate-x-[50%] duration-300 bg-white w-[90%] md:w-[75%] rounded-lg font-inter p-4 text-black ${
+                selectSizeOpen.productId && selectSizeOpen.isSelectSizeOpen
+                  ? "block"
+                  : "hidden"
+              } `}
+            >
+              <section className="flex items-center justify-between">
+                <h4 className="text-[14px] text-[#101828]">Select size</h4>
+                <MdClose
+                  size={20}
+                  className="text-[#1d2939]"
+                  onClick={() =>
+                    setSelectSizeOpen({
+                      productId: null,
+                      isSelectSizeOpen: false,
+                    })
                   }
-                })
-              : "No sizes"}
+                />
+              </section>
+
+              <section className="border-none w-fit sm2:w-full grid grid-cols-3 gap-5 sm2:gap-3 my-4">
+                {sizes
+                  ? sizes.map((size) => {
+                      if (size.selected) {
+                        return (
+                          <section
+                            key={size.id}
+                            className="grid place-items-center  cursor-pointer text-[16px] font-medium w-[40px] h-[40px] bg-[#101828] text-[#fcfcfd] rounded-lg"
+                            onClick={() => handleSize(size.id)}
+                          >
+                            {size.size}
+                          </section>
+                        );
+                      } else if (size.disabled) {
+                        return (
+                          <section
+                            key={size.id}
+                            className={
+                              "grid place-items-center border-[1px] border-[#d0d5dd] cursor-not-allowed text-[16px] font-medium w-[43px] h-[40px] bg-[#eaecf0] text-[#667085] rounded-lg"
+                            }
+                          >
+                            {size.size}
+                          </section>
+                        );
+                      } else {
+                        return (
+                          <section
+                            key={size.id}
+                            className={
+                              "grid place-items-center border-[1px] border-[#d0d5dd] cursor-pointer text-[16px] font-medium w-[43px] h-[40px] bg-white rounded-lg"
+                            }
+                            onClick={() => handleSize(size.id, items?.id)}
+                          >
+                            {size.size}
+                          </section>
+                        );
+                      }
+                    })
+                  : "No sizes"}
+              </section>
+
+              <button
+                className="bg-black rounded-lg font-inter text-[14px] text-white px-5 py-2 w-full block mx-auto cursor-pointer duration-300"
+                onClick={() => handleAddToBag(items)}
+              >
+                Add to bag
+              </button>
+            </section>
           </section>
 
-          <button
-            className="bg-black rounded-lg font-inter text-[14px] text-white px-5 py-2 w-full block mx-auto cursor-pointer duration-300"
-            onClick={() => handleAddToBag(item)}
-          >
-            Add to bag
-          </button>
+          <section className="w-full flex items-start justify-between mt-5">
+            <Link
+              to={`/p?type=${items?.category_slug}&item?=${items?.slug_name}`}
+              className="w-full"
+            >
+              <section className="whitespace-normal">
+                <h3 className="text-[12px] md:text-[14px] font-medium">
+                  {items?.product_name}
+                </h3>
+                <p className="text-[20px] font-anybody font-bold mt-3">
+                  {priceFormatter(items?.price)}
+                </p>
+              </section>
+            </Link>
+
+            <Heart variant="Linear" size={24} className="text-black ml-6" />
+          </section>
         </section>
-      </section>
-
-      <section className="w-full flex items-start justify-between mt-5">
-        <Link
-          to={`/p?type=${item.category_slug}&item=${item.slug_name}`}
-          className="w-full"
-        >
-          <section className="whitespace-normal">
-            <h3 className="text-[12px] md:text-[14px] font-medium">
-              {item.product_name}
-            </h3>
-            <p className="text-[20px] font-anybody font-bold mt-3">
-              {priceFormatter(item.price)}
-            </p>
-          </section>
-        </Link>
-
-        <Heart variant="Linear" size={24} className="text-black ml-6" />
-      </section>
-    </section>
+      ) : (
+        <>
+          {" "}
+          <Loader />{" "}
+        </>
+      )}
+    </>
   );
 };
 
